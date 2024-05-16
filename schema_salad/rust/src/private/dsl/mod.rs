82e772fda09dd91a8b6e2437c10dff6e2210b864
Giuseppe Eletto <giuseppe@eletto.me>
2024-05-16 17:24:34 +0200

    refactor(rust): dsl preprocessor

--- schema_salad/rust/src/private/dsl/mod.rs
+++ schema_salad/rust/src/private/dsl/mod.rs
@@ -24,18 +24,54 @@ impl<T> Preprocessor<T> {
 
 impl<'de, V: de::Visitor<'de>> Preprocessor<V> {
     fn __visit_map<A: de::MapAccess<'de>>(self, map: A) -> Result<V::Value, A::Error> {
+        // Util functions...
+        fn get_uri_filename(uri: &str) -> (&str, Option<&str>) {
+            let query_frag_idx = uri.find(['?', '#']).unwrap_or(uri.len());
+            let uri = &uri[..query_frag_idx];
+            let path_separator_idx = uri.rfind(['/', '\\']).map(|v| v + 1).unwrap_or(0);
+
+            let file = &uri[path_separator_idx..];
+            let ext = file
+                .find('.')
+                .and_then(|dot_idx| (dot_idx < file.len() - 1).then_some(&file[dot_idx + 1..]));
+
+            (file, ext) // FIXME file could be an empty string
+        }
+
+        #[cfg_attr(not(feature = "dsl_http"), inline)]
+        fn read_uri_content<E: de::Error>(uri: &str) -> Result<String, E> {
+            #[cfg(feature = "dsl_http")]
+            if uri.starts_with("https://") || uri.starts_with("http://") {
+                return match ureq::get(uri).call() {
+                    Ok(r) if r.status() == 200 => r
+                        .into_string()
+                        .map_err(|e| E::custom(format_args!("preprocessor error: {e}"))),
+
+                    Ok(r) => Err(E::custom(format_args!(
+                        "preprocessor error: request to `{uri}` failed with code {}: {}",
+                        r.status(),
+                        r.status_text(),
+                    ))),
+                    Err(e) => Err(E::custom(format_args!("preprocessor error: {e}"))),
+                };
+            }
+
+            fs::read_to_string(uri).map_err(|e| E::custom(format_args!("preprocessor error: {e}")))
+        }
+
+        // "__visit_map" method...
         let mut map = PeekableMapAccess::new(map);
         let key_value = map.peek_key::<CompactString>()?;
 
         match key_value.as_deref() {
             Some("$import") => {
                 let uri = map.next_value::<CompactString>()?;
-                let (filename, extension) = util::get_uri_filename(&uri);
+                let (filename, extension) = get_uri_filename(&uri);
 
                 match extension {
                     #[cfg(all(feature = "dsl_json", feature = "dsl_http"))]
                     Some("json" | "JSON") | None => {
-                        let content = util::read_uri_content(&uri)?;
+                        let content = read_uri_content(&uri)?;
                         self.deserialize_json(filename, content)
                     }
                     #[cfg(all(feature = "dsl_json", not(feature = "dsl_http")))]
@@ -45,7 +81,7 @@ impl<'de, V: de::Visitor<'de>> Preprocessor<V> {
                     }
                     #[cfg(feature = "dsl_yaml")]
                     Some("yaml" | "yml" | "YAML" | "YML") => {
-                        let content = util::read_uri_content(&uri)?;
+                        let content = read_uri_content(&uri)?;
                         self.deserialize_yaml(filename, content)
                     }
                     Some(ext) => Err(<A::Error as de::Error>::custom(format_args!(
@@ -59,7 +95,7 @@ impl<'de, V: de::Visitor<'de>> Preprocessor<V> {
             }
             Some("$include") => {
                 let uri = map.next_value::<CompactString>()?;
-                let content = util::read_uri_content(&uri)?;
+                let content = read_uri_content(&uri)?;
                 self.delegate.visit_string(content)
             }
             _ => self.delegate.visit_map(Preprocessor::__new(map)),
@@ -86,43 +122,5 @@ impl<'de, V: de::Visitor<'de>> Preprocessor<V> {
         let deserializer = serde_yaml::Deserializer::from_reader(data_cursor);
         de::Deserializer::deserialize_any(deserializer, self)
             .map_err(|e| E::custom(format_args!("preprocessor error: {e}")))
-    }
-}
-
-mod util {
-    use super::*;
-
-    pub(super) fn get_uri_filename(uri: &str) -> (&str, Option<&str>) {
-        let query_frag_idx = uri.find(['?', '#']).unwrap_or(uri.len());
-        let uri = &uri[..query_frag_idx];
-        let path_separator_idx = uri.rfind(['/', '\\']).map(|v| v + 1).unwrap_or(0);
-
-        let file = &uri[path_separator_idx..];
-        let ext = file
-            .find('.')
-            .and_then(|dot_idx| (dot_idx < file.len() - 1).then_some(&file[dot_idx + 1..]));
-
-        (file, ext) // FIXME file could be an empty string
-    }
-
-    #[cfg_attr(not(feature = "dsl_http"), inline)]
-    pub(super) fn read_uri_content<E: de::Error>(uri: &str) -> Result<String, E> {
-        #[cfg(feature = "dsl_http")]
-        if uri.starts_with("https://") || uri.starts_with("http://") {
-            return match ureq::get(uri).call() {
-                Ok(r) if r.status() == 200 => r
-                    .into_string()
-                    .map_err(|e| E::custom(format_args!("preprocessor error: {e}"))),
-
-                Ok(r) => Err(E::custom(format_args!(
-                    "preprocessor error: request to `{uri}` failed with code {}: {}",
-                    r.status(),
-                    r.status_text(),
-                ))),
-                Err(e) => Err(E::custom(format_args!("preprocessor error: {e}"))),
-            };
-        }
-
-        fs::read_to_string(uri).map_err(|e| E::custom(format_args!("preprocessor error: {e}")))
     }
 }
